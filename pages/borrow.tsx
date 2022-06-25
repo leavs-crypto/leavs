3dd4b3cad20e3be3459734c409b660872aa1f1c1
Fernando <f.rodriguez.hervias@gmail.com>
2022-06-25 16:35:30 -0400

    commit with no yarn

--- pages/borrow.tsx
+++ pages/borrow.tsx
@@ -1,61 +1,17 @@
 import {
-    Box,
     Button,
-    FormControl,
-    FormLabel,
-    Input,
-    Container,
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    useDisclosure
+    Container
 } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import Table from "../components/Table";
 import Drawer from "../components/Drawer";
+import BorrowModal from "../components/BorrowModal";
 
 
 const Borrow: NextPage = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure();
-
     return (
         <Drawer parent="borrow">
-            <Container centerContent>
-                <Button onClick={onOpen}>Borrow</Button>
-            </Container>
-
-            <Modal
-                isOpen={isOpen}
-                onClose={onClose}
-            >
-                <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>Borrow your account</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody pb={6}>
-                        <FormControl>
-                            <FormLabel>First name</FormLabel>
-                            <Input placeholder='First name' />
-                        </FormControl>
-
-                        <FormControl mt={4}>
-                            <FormLabel>Last name</FormLabel>
-                            <Input placeholder='Last name' />
-                        </FormControl>
-                    </ModalBody>
-
-                    <ModalFooter>
-                        <Button colorScheme='blue' mr={3}>
-                            Save
-                        </Button>
-                        <Button onClick={onClose}>Cancel</Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
+            <BorrowModal/>
         </Drawer>
     );
 };
