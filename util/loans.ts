4d9669838f4c06fcd2984927f5c35d5a46178cc7
Alexander Ludwig <alex@Alexanders-MacBook-Pro-3.local>
2022-06-26 00:06:11 -0400

    integration work

--- util/loans.ts
+++ util/loans.ts
@@ -2,6 +2,8 @@ import WalletConnectProvider from "@walletconnect/web3-provider";
 import { ethers, providers } from "ethers";
 import LoanABI from "../artifacts/contracts/Loan.sol/Loan.json";
 
+import {createFlow, updateFlow, getFlowRate} from "../util/superfluid";
+
 
 
 export async function createLoanContract(
@@ -18,8 +20,36 @@ export async function createLoanContract(
       LoanABI.bytecode,
       ethersProvider.getSigner()
     );
-    const contract = await factory.deploy(worldCoinCID, loanAmount, loanTerm, APR);
-  } catch (e) {
+    console.log('Deploying contract...')
+    const contract = await factory.deploy(worldCoinCID, loanAmount, loanTerm, APR,  {gasLimit:800000});
+    console.log(contract.address)
+    return contract.address
+} catch (e) {
     console.error(e);
   }
 }
+
+
+export async function requestLoan(
+    borrower: string,
+    provider: WalletConnectProvider,
+    worldCoinCID: string,
+    loanAmount: number,
+    loanTerm: number,
+    APR:number
+  ) 
+  {
+
+    let totalLoan = 1000; // TODO calculate real loan
+    const deployedAddress = createLoanContract(provider, worldCoinCID, loanAmount, loanTerm, APR)
+    // save the deployed address in the Leavs depled smart contract
+
+    // open up the Superfluid stream
+    let flowRate= totalLoan/loanTerm/30/24/60/60
+    //await createFlow(borrower, String(flowRate), provider)
+    
+
+    // put up the NFT on OpenSea marketplace
+
+  }
+  
