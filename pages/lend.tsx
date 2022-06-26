7aa64a169d19450d660c3363fb6dd0f42056a6f8
Fernando <f.rodriguez.hervias@gmail.com>
2022-06-26 07:41:35 -0400

    in process

--- pages/lend.tsx
+++ pages/lend.tsx
@@ -6,7 +6,6 @@ import Table from "../components/Table";
 const Borrow: NextPage = () => {
     return (
         <Drawer parent="lend">
-            <Box textAlign="center">Lend page</Box>
             <Table />
         </Drawer>
     );
