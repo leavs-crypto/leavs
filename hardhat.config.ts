56b184a4dc9ade86f05953685789194505ad8698
Fernando <f.rodriguez.hervias@gmail.com>
2022-06-25 23:49:26 -0400

    pocket network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,12 +9,18 @@ dotenv.config();
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
+  defaultNetwork: "pokt",
   solidity: "0.8.13",
   typechain: {
     outDir: "artifacts/contracts/types",
     target: "ethers-v5",
   },
   networks: {
+    pokt: {
+      url: "https://poly-archival.gateway.pokt.network/v1/lb/62b7cdfa123e6f0039851f92",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
       accounts:
