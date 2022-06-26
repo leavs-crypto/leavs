86818d25e8caf15613a65d6323755c0404b0626b
Justin <jusheinz@aol.de>
2022-06-26 03:16:53 -0400

    nife

--- util/opensea.ts
+++ util/opensea.ts
@@ -1,40 +1,50 @@
-import { useContext } from "react";
 import divide from "divide-bigint";
+export const sellNFT = async (
+  ownerAddress: string,
+  tokenAddress: string,
+  tokenId: number,
+  days: number,
+  amount: number
+) => {
+  // Expire this auction one day from now.
+  // Note that we convert from the JavaScript timestamp (milliseconds):
 
-import { OpenSeaSDK, Network } from 'opensea-js'
-
-
-
-import { AuthContext } from "../components/WithWalletConnect";
-const provider = useContext(AuthContext);
-
-const openseaSDK = new OpenSeaSDK(provider, {
-    networkName: Network.Main,
-    apiKey: 'YOUR_API_KEY'
-  })
-
-  export const sellNFT = async (ownerAddress: string, tokenAaddress: string, tokenId: number, days: number, amount: number) => {
-  
-    // Expire this auction one day from now.
-    // Note that we convert from the JavaScript timestamp (milliseconds):
-    
-    const expirationTime = Math.round(Date.now() / 1000 + 60 * 60 * 24 * days)
-
-    const listing = await openseaSDK.createSellOrder({
-        asset: {
-            tokenId,
-            tokenAaddress,
-        },
-        ownerAddress,
-        amount: 3,
-        // If `endAmount` is specified, the order will decline in value to that amount until `expirationTime`. Otherwise, it's a fixed-price order:
-        expirationTime
-    })
+  const expirationTime = Math.round(Date.now() / 1000 + 60 * 60 * 24 * days);
+const data= {
+    offerer:ownerAddress,
+    zone:"?",
+    zoneHash:"??",
+    start_time:Date.now(),
+    endTime:expirationTime,
+    orderType:1,
+offer:{
+    item_type:1,
+    token:tokenAddress,
+    startAmount:100,
+    endAmount:100
 }
+}
+  }
+  const url = `https://testnets-api.opensea.io/v2/orders/ethereum/seaport/listings`;
+  try {
+    const resp = await axios.post(url, data, {
+      headers: {
+        "content-type": "application/json",
+        "x-api-key": API_KEY,
+      },
+    });
+    return resp.data.IpfsHash;
+};
 
 export const buyNFT = async (ownerAddress: string, tokenAddress: string) => {
-    const order = await openseaSDK.api.getOrder({ protocol: 'seaport', side: "ask" })
-    //ownerAddress The buyer's wallet address, also the taker
-    const transactionHash = await openseaSDK.fulfillOrder({ order, accountAddress: ownerAddress })
-    console.log(transactionHash)
-}
+  const order = await openseaSDK.api.getOrder({
+    protocol: "seaport",
+    side: "ask",
+  });
+  //ownerAddress The buyer's wallet address, also the taker
+  const transactionHash = await openseaSDK.fulfillOrder({
+    order,
+    accountAddress: ownerAddress,
+  });
+  console.log(transactionHash);
+};
